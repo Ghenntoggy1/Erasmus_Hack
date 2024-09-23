e8dc19a0d5e184e2800086e04cede45a50caa682
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-09-23 21:58:20 +0300

    feature: Add endpoint for UniversityController that gets University by ID.

--- src/main/java/org/example/University/UniversityController.java
+++ src/main/java/org/example/University/UniversityController.java
@@ -1,12 +1,17 @@
 package org.example.University;
 
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/universities")
 public class UniversityController {
     private final UniversityService universityService;
@@ -18,5 +23,10 @@ public class UniversityController {
     @GetMapping()
     public List<UniversityDTO> getUniversities() {
         return universityService.getUniversities();
+    }
+
+    @GetMapping("/univ_id:{id}")
+    public UniversityDTO getUniversityById(@PathVariable("id") Integer id) {
+        return universityService.getUniversityById(id);
     }
 }
